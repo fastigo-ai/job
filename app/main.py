b286d3baeb7f94dee058449455a0981f634e8c74
snahangshu mallik <mallik00bis@gmail.com>
2026-02-23 19:16:06 +0530

    fourth commit

--- app/main.py
+++ app/main.py
@@ -1,14 +1,4 @@
 from fastapi import FastAPI
-from app.database import engine
-from app.models import Base
-from app.routers import admin, jobs
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Career Connect Backend")
-
-app.include_router(admin.router)
-app.include_router(jobs.router)from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from app.database import engine
